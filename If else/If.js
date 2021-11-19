9e8e37bffb7e9b5e8033efe0d0b7b614397efe3c
VictoriaVorobets <91741883+VictoriaVorobets@users.noreply.github.com>
2021-11-19 09:54:50 +0200

    Add files via upload

--- If else/If.js
+++ If else/If.js
@@ -1,6 +1,6 @@
 // Перевірка на пароль
-const пароль = prompt('Введіть пароль');
-if (пароль == 787898) {
+const password = prompt('Введіть пароль');
+if (password == 787898) {
     console.log('Пароль вірний');
 } else {
     console.log('Хибний пароль');
@@ -9,34 +9,35 @@ if (пароль == 787898) {
 
 // Кількість столів
 let number = prompt('Введіть число');
+let remainderByTen = number % 10;
+let remainderByOneHundred = number % 100;
+// console.log(Остача від ділення + remainderByTen);
+
+if (number >= 1) {
+    if (remainderByOneHundred > 11 && remainderByOneHundred < 19) {
+        console.log(number + 'столів');
+    } else if (remainderByTen == 1) {
+        console.log(number + 'стіл');
+    } else if (remainderByTen >= 2 && remainderByTen <= 4) {
+        console.log(number + 'столи');
+    } else {
+        console.log(number + 'столів');
+    }
 
 
-if (number == 1 && number + 1) {
-    console.log(number + 'стіл');
-} else if (number == 2 && number == 3 && number == 4 && number + 2 && number + 3 && number + 4){
-    console.log(number + 'столи');
-}
- else {
-    console.log(number + 'столів')
-}
+    // Повідомлення відносно ролі
+    let name = prompt('Як вас звати?');
+    if (name == 'Ігор')
+        alert('Привіт, адмін ' + name);
 
-
-
-
-
-// Повідомлення відносно ролі
-let name = prompt('Як вас звати?');
-if (name == 'Ігор')
-    alert('Привіт, адмін ' + name);
-
-if (name == 'Сергій') {
-    alert('Привіт, адмін ' + name);
-}
-if (name == 'Паша') {
-    alert('Привіт, хворий ' + name);
-}
-if (name == 'Оля') {
-    alert('Вітання Вам, ' + name);
-} else if (name !== 'Ігор' && name !== 'Сергій' && name !== 'Паша' && name !== 'Оля') {
-    console.log('Постороннім вхід заборонено, коронавірус');
-}
+    if (name == 'Сергій') {
+        alert('Привіт, адмін ' + name);
+    }
+    if (name == 'Паша') {
+        alert('Привіт, хворий ' + name);
+    }
+    if (name == 'Оля') {
+        alert('Вітання Вам, ' + name);
+    } else if (name !== 'Ігор' && name !== 'Сергій' && name !== 'Паша' && name !== 'Оля') {
+        console.log('Постороннім вхід заборонено, коронавірус');
+    }
