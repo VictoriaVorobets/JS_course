725d6dce073d430f09e9adad7527d040c5762393
VictoriaVorobets <91741883+VictoriaVorobets@users.noreply.github.com>
2021-11-19 23:00:32 +0200

    Add files via upload

---
+++ Loops/Loops.js
@@ -0,0 +1,68 @@
+// Трикутник1
+// let result = ' ';
+// for (let i = 0; i < 10; i++) {
+//     result += "*";
+
+//     console.log(result);
+// }
+
+
+// Прямокутник2
+// let result = '';
+// for (let i = 0; i < 10; i++) {
+//     for (let k = 0; k < 10; k++) {
+//         result += "*";
+//     }
+//     console.log(result);
+//     result = "";
+// }
+
+
+// Квадрат3
+// let result = '';
+// for (let i = 0; i < 10; i++) {
+//     for (let k = 0; k < 10; k++) {
+//         result += '*  ';
+//     }
+//     console.log(result);
+//     result = "";
+// }
+
+// Трикутник перевернутий 4
+// let n = 10;
+// let result = '';
+// for (let i = 0; i < n; i++) {
+//     for (k = 0; k < n - i; k++) {
+//         result += "*";
+//     }
+//     console.log(result);
+//     result = "";
+// }
+
+// трикутник5
+// let n = 10;
+// let result = "";
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 0; j < n - i; j++) {
+//         result += " ";
+//     }
+//     for (let k = 0; k < i; k++) {
+//         result += "*";
+//     }
+//     console.log(result);
+//     result = "";
+// }
+
+// Трикутник6
+
+// let result = "";
+// for (let i = 1; i < 11; i++) {
+//     for (let j = 1; j < i; j++) {
+//         result = "";
+//     }
+//     for (let k = 11; k > i; k--) {
+//         result += "*";
+//     }
+//     console.log(result);
+//     result = '';
+// }
